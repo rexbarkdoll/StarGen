1b26d94f639296a0b73249bf1947427634691979
Graham Reeds <graham.reeds@gmail.com>
2013-11-20 21:54:25 +0000

    Added TestStarGenDemo project and set up so it compiles and runs the tests as a post-build step.

---
+++ TestStarGenDemo/TestStarGenDemo.cpp
@@ -0,0 +1,10 @@
+// TestStarGenDemo.cpp : Defines the entry point for the console application.
+//
+
+#include "UnitTest++.h"
+
+int main(int argc, char* argv[])
+{
+	return UnitTest::RunAllTests();
+}
+
